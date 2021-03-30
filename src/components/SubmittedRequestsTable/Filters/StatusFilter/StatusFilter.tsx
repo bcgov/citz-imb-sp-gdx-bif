e44a557f826c1f0b446b72a369e03780e5d4dcf5
SDToews <33942967+SDToews@users.noreply.github.com>
2021-03-30 13:33:11 -0700

    fixed navbar and filter

--- src/components/SubmittedRequestsTable/Filters/StatusFilter/StatusFilter.tsx
+++ src/components/SubmittedRequestsTable/Filters/StatusFilter/StatusFilter.tsx
@@ -1,97 +1,118 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Toggle, IColumn, Stack } from "@fluentui/react";
+import React, { useState, useEffect } from 'react';
+import { Toggle, IColumn, Stack } from '@fluentui/react';
 
 interface StatusFilterTypes {
-  data: Array<any>;
-  columns: Array<IColumn>;
-  setFilter: Function;
+	data: Array<any>;
+	columns: Array<IColumn>;
+	setFilter: Function;
 }
 
 interface statusOptionsTypes {
-  status?: string;
-  checked?: boolean;
+	status?: string;
+	checked?: boolean;
 }
 
-export const StatusFilter = ({
-  data,
-  columns,
-  setFilter,
-}: StatusFilterTypes) => {
-  const [statusOptions, setStatusOptions] = useState<statusOptionsTypes[]>([]);
+export const StatusFilter = (props: StatusFilterTypes) => {
+	const { data, columns, setFilter } = props;
 
-  useEffect(() => {
-    //get all the Status values being used in the data
-    if (data !== undefined) {
-      //@ts-ignore //!because React-Query is not properly typed
-      const TableStatusOptions = data.map(
-        //@ts-ignore //!because React-Query is not properly typed
-        (item) => item.Status
-      );
-    }
+	const [statusOptions, setStatusOptions] = useState<statusOptionsTypes[]>(
+		[]
+	);
 
-    //get rid of duplicate Status values
-    // const statusSet = [...new Set(TableStatusOptions)];
+	useEffect(() => {
+		//get all the Status values being used in the data
+		if (data !== undefined) {
+			//@ts-ignore //!because React-Query is not properly typed
+			const TableStatusOptions = data.map(
+				//@ts-ignore //!because React-Query is not properly typed
+				(item) => item.Status
+			);
 
-    //  const handleFilterClick = (event: any) => {
+			// get rid of duplicate Status values
 
-    // 		const status = statusSet.map((status) => {
-    // 			console.log('status :>> ', status);
-    // 			let checked = true;
-    // 			if (status === 'Closed' || status === 'Rejected') {
-    // 				checked = false;
-    // 			}
-    // 			return { status, checked };
-    // 		});
-    // 		status.push({ status: 'Show All', checked: false });
-    // 		//@ts-ignore //!because Set is not properly typed
-    // 		setStatusOptions(status);
-    // 	}
-  }, []);
+			const statusSet = [...new Set(TableStatusOptions)];
 
-  useEffect(() => {
-    //when the statusOptions change, get all the ones that are checked
-    const filterValues = statusOptions
-      .filter((option) => !option.checked)
-      .map((option) => option.status);
+			setStatusOptions((prevState) => {
+				let tempArray: statusOptionsTypes[] = [];
 
-    //filter the data based on the checked statusOptions
-    setFilter("Status", filterValues);
+				for (let i = 0; i < statusSet.length; i++) {
+					const index = prevState.findIndex(
+						(option) => option.status === statusSet[i]
+					);
 
-    return () => {};
-  }, [statusOptions]);
+					if (index > -1) {
+						tempArray.push(prevState[index]);
+					} else {
+						if (
+							statusSet[i] === 'Closed' ||
+							statusSet[i] === 'Rejected'
+						) {
+							tempArray.push({
+								status: statusSet[i],
+								checked: false,
+							});
+						} else {
+							tempArray.push({
+								status: statusSet[i],
+								checked: true,
+							});
+						}
+					}
+				}
 
-  const handleFilterChange = (status: string, checked?: boolean) => {
-    console.log("status, checked :>> ", status, checked);
-    // update the button state to show that whether it is in effect
-    let newRequestStates = statusOptions.map((thisStatus) => {
-      if (thisStatus.status === status)
-        return {
-          status,
-          checked: checked,
-        };
-      return thisStatus;
-    });
+				return tempArray;
+			});
+		}
+	}, [data]);
 
-    setStatusOptions(newRequestStates);
-  };
+	useEffect(() => {
+		//when the statusOptions change, get all the ones that are checked
+		const filterValues = statusOptions
+			.filter((option) => option.checked)
+			.map((option) => option.status);
 
-  return (
-    <Stack horizontal>
-      {statusOptions.map((statusContainer: any) => {
-        return (
-          <Toggle
-            key={statusContainer.status}
-            label={statusContainer.status}
-            checked={statusContainer.checked}
-            onText="On"
-            offText="Off"
-            onChange={(
-              event: React.MouseEvent<HTMLElement>,
-              checked?: boolean
-            ) => handleFilterChange(statusContainer.status, checked)}
-          />
-        );
-      })}
-    </Stack>
-  );
+		//filter the data based on the checked statusOptions
+		setFilter('Status', filterValues);
+
+		return () => {};
+	}, [statusOptions]);
+
+	const handleFilterChange = (status: string, checked: boolean) => {
+		// update the button state to show that whether it is in effect
+		let newRequestStates = statusOptions.map((thisStatus) => {
+			if (thisStatus.status === status)
+				return {
+					status,
+					checked: checked,
+				};
+			return thisStatus;
+		});
+
+		setStatusOptions(newRequestStates);
+	};
+
+	return (
+		<Stack horizontal>
+			{statusOptions.map((statusContainer: any) => {
+				return (
+					<Toggle
+						key={statusContainer.status}
+						label={statusContainer.status}
+						checked={statusContainer.checked}
+						onText='On'
+						offText='Off'
+						onChange={(
+							event: React.MouseEvent<HTMLElement>,
+							checked?: boolean
+						) =>
+							handleFilterChange(
+								statusContainer.status,
+								checked ?? false
+							)
+						}
+					/>
+				);
+			})}
+		</Stack>
+	);
 };
--- src/components/SubmittedRequestsTable/SubmittedRequestsTable.tsx
+++ src/components/SubmittedRequestsTable/SubmittedRequestsTable.tsx
@@ -1,28 +1,29 @@
-import { IColumn } from "@fluentui/react";
+import { IColumn } from '@fluentui/react';
 
-import { ISubmittedRequestItem } from "./ISubmittedRequestItem";
-import { testData } from "./testData";
-import React, { useEffect, useMemo, useState } from "react";
-import { GetSubmittedRequests } from "components/API/GET/GetSubmittedRequests";
-import { AddItemsToList } from "components/ApiCalls";
-import { useQuery, useQueryClient, useMutation } from "react-query";
+import { ISubmittedRequestItem } from './ISubmittedRequestItem';
+import { testData } from './testData';
+import React, { useEffect, useMemo, useState } from 'react';
+import { GetSubmittedRequests } from 'components/API/GET/GetSubmittedRequests';
+import { AddItemsToList } from 'components/ApiCalls';
+import { useQuery, useQueryClient, useMutation } from 'react-query';
 
 import {
-  useTable,
-  useSortBy,
-  useFilters,
-  useGlobalFilter,
-  useAsyncDebounce,
-  Row,
-} from "react-table";
-import { DetailsList } from "@fluentui/react";
-import { initializeIcons } from "@fluentui/react/lib/Icons";
-import { GetColumns } from "components/API/GET/GetColumns";
-import { tableSort } from "./tableSort";
-import { statusColumnFilter } from "./Filters/StatusFilter/statusColumnFilter";
-import { GlobalFilter } from "./Filters/GlobalFilter";
-import { StatusFilter } from "./Filters/StatusFilter/StatusFilter";
-
+	useTable,
+	useSortBy,
+	useFilters,
+	useGlobalFilter,
+	useAsyncDebounce,
+	Row,
+} from 'react-table';
+import { DetailsList } from '@fluentui/react';
+import { initializeIcons } from '@fluentui/react/lib/Icons';
+import { GetColumns } from 'components/API/GET/GetColumns';
+import { tableSort } from './tableSort';
+import { statusColumnFilter } from './Filters/StatusFilter/statusColumnFilter';
+import { GlobalFilter } from './Filters/GlobalFilter';
+import { StatusFilter } from './Filters/StatusFilter/StatusFilter';
+import { IntakeForm } from 'components/IntakeForm/IntakeForm';
+import { NavBar } from './NavBar/NavBar';
 //!because React-Table is not properly typed
 // import { QuerySuccessResult } from "react-query";
 // To intialize
@@ -39,125 +40,126 @@ initializeIcons(undefined, { disableWarnings: true });
 */
 
 export const SubmittedRequestsTable = () => {
-  const listName = "Submitted Requests";
+	const listName = 'Submitted Requests';
 
-  //@ts-ignore //!because React-Table is not properly typed
-  const query: any = useQuery(listName, GetSubmittedRequests);
+	//@ts-ignore //!because React-Table is not properly typed
+	const query: any = useQuery(listName, GetSubmittedRequests);
 
-  const queryClient: any = useQueryClient();
+	const queryClient: any = useQueryClient();
 
-  const addItemMutation = useMutation(
-    (newItem: ISubmittedRequestItem) =>
-      AddItemsToList({
-        listName,
-        items: newItem,
-      }),
-    {
-      onMutate: async (newItem: ISubmittedRequestItem) => {
-        await queryClient.cancelQueries(listName);
+	const addItemMutation = useMutation(
+		(newItem: ISubmittedRequestItem) =>
+			AddItemsToList({
+				listName,
+				items: newItem,
+			}),
+		{
+			onMutate: async (newItem: ISubmittedRequestItem) => {
+				await queryClient.cancelQueries(listName);
 
-        const previousValues = queryClient.getQueryData(listName);
+				const previousValues = queryClient.getQueryData(listName);
 
-        //@ts-ignore //!react-query is not typed
-        queryClient.setQueryData(listName, (oldValues) => {
-          let newValues = [...oldValues.items];
+				//@ts-ignore //!react-query is not typed
+				queryClient.setQueryData(listName, (oldValues) => {
+					let newValues = [...oldValues.items];
 
-          newValues.push(newItem);
-          return { listInfo: oldValues.listInfo, items: newValues };
-        });
+					newValues.push(newItem);
+					return { listInfo: oldValues.listInfo, items: newValues };
+				});
 
-        return { previousValues };
-      },
-      //@ts-ignore //!react-query is not typed
-      onError: (error, newItem: ISubmittedRequestItem, context) =>
-        queryClient.setQueryData(listName, context?.previousValues),
-      onSettled: async () => await queryClient.invalidateQueries(listName),
-    }
-  );
+				return { previousValues };
+			},
+			//@ts-ignore //!react-query is not typed
+			onError: (error, newItem: ISubmittedRequestItem, context) =>
+				queryClient.setQueryData(listName, context?.previousValues),
+			onSettled: async () =>
+				await queryClient.invalidateQueries(listName),
+		}
+	);
 
-  const data = useMemo(() => {
-    if (query.isLoading || query.isError) return [];
+	const data = useMemo(() => {
+		if (query.isLoading || query.isError) return [];
 
-    return query.data.items;
-  }, [query.isLoading, query.isError, query.data?.items]);
+		return query.data.items;
+	}, [query.isLoading, query.isError, query.data?.items]);
 
-  const columns: IColumn & any = useMemo(() => {
-    if (query.isLoading || query.isError) return [];
+	const columns: IColumn & any = useMemo(() => {
+		if (query.isLoading || query.isError) return [];
 
-    const initialColumns = GetColumns(
-      query.data.listInfo.Columns,
-      query.data.listInfo.Fields.results
-    );
+		const initialColumns = GetColumns(
+			query.data.listInfo.Columns,
+			query.data.listInfo.Fields.results
+		);
 
-    //we need to treat 'Status' column differently as we are going to filter on it
-    //get the 'Status' column
-    let statusColumn = initialColumns.filter(
-      (column) => column.key === "Status"
-    )[0];
+		//we need to treat 'Status' column differently as we are going to filter on it
+		//get the 'Status' column
+		let statusColumn = initialColumns.filter(
+			(column) => column.key === 'Status'
+		)[0];
 
-    //set the custom filter functionality on 'Status' column
-    //@ts-ignore //!because React-Table is not properly typed
-    statusColumn.filter = statusColumnFilter;
+		//set the custom filter functionality on 'Status' column
+		//@ts-ignore //!because React-Table is not properly typed
+		statusColumn.filter = statusColumnFilter;
 
-    //add the modified 'Status' column back in with the other columns
-    const modifiedColumns = [
-      ...initialColumns.filter((column) => column.key !== "Status"),
-      statusColumn,
-    ];
+		//add the modified 'Status' column back in with the other columns
+		const modifiedColumns = [
+			...initialColumns.filter((column) => column.key !== 'Status'),
+			statusColumn,
+		];
 
-    return modifiedColumns;
-  }, [query.isLoading, query.isError, query.data]);
+		return modifiedColumns;
+	}, [query.isLoading, query.isError, query.data]);
 
-  const tableInstance: any = useTable(
-    {
-      columns,
-      data,
-      disableSortRemove: true,
-      autoResetSortBy: false,
-      autoResetGlobalFilter: false,
-    },
-    useFilters,
-    useGlobalFilter,
-    useSortBy
-  );
+	const tableInstance: any = useTable(
+		{
+			columns,
+			data,
+			disableSortRemove: true,
+			autoResetSortBy: false,
+			autoResetGlobalFilter: false,
+		},
+		useFilters,
+		useGlobalFilter,
+		useSortBy
+	);
 
-  // When a user clicks a column sort by it
-  const handleColumnClick = (ev: any, column: any) => {
-    tableSort(ev, column, tableInstance);
-  };
+	// When a user clicks a column sort by it
+	const handleColumnClick = (ev: any, column: any) => {
+		tableSort(ev, column, tableInstance);
+	};
 
-  if (query.isLoading) return <div>loading...</div>;
+	if (query.isLoading) return <div>loading...</div>;
 
-  if (query.isError) return <div>{query.error}</div>;
+	if (query.isError) return <div>{query.error}</div>;
 
-  const addNewRequest = () => {
-    console.log("add new request");
-    addItemMutation.mutateAsync(testData);
-  };
+	const addNewRequest = () => {
+		console.log('add new request');
+		addItemMutation.mutateAsync(testData);
+	};
 
-  return (
-    <>
-      <div>{query.status}</div>
-      <br />
-      <GlobalFilter
-        preGlobalFilteredRows={tableInstance.preGlobalFilteredRows}
-        globalFilter={tableInstance.state.globalFilter}
-        setGlobalFilter={tableInstance.setGlobalFilter}
-        useAsyncDebounce={useAsyncDebounce}
-      />
-      <StatusFilter
-        data={tableInstance.data}
-        columns={tableInstance.columns}
-        setFilter={tableInstance.setFilter}
-      />
-
-      <DetailsList
-        items={tableInstance.sortedRows.map((row: Row) => row.values)}
-        columns={tableInstance.columns}
-        onColumnHeaderClick={handleColumnClick}
-        selectionMode={0}
-        checkboxVisibility={2}
-      />
-    </>
-  );
+	return (
+		<>
+			<NavBar addNewRequest={addNewRequest}>
+				<IntakeForm />
+				<GlobalFilter
+					preGlobalFilteredRows={tableInstance.preGlobalFilteredRows}
+					globalFilter={tableInstance.state.globalFilter}
+					setGlobalFilter={tableInstance.setGlobalFilter}
+					useAsyncDebounce={useAsyncDebounce}
+				/>
+				<StatusFilter
+					data={tableInstance.data}
+					columns={tableInstance.columns}
+					setFilter={tableInstance.setFilter}
+				/>
+			</NavBar>
+			<DetailsList
+				items={tableInstance.sortedRows.map((row: Row) => row.values)}
+				columns={tableInstance.columns}
+				onColumnHeaderClick={handleColumnClick}
+				selectionMode={0}
+				checkboxVisibility={2}
+			/>
+		</>
+	);
 };

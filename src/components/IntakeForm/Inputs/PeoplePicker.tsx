ffa9230072085aacc393471072aab495454c38a3
adam spiteri <adam.spiteri@gov.bc.ca>
2021-03-31 10:28:20 -0700

    started people picker part 2

---
+++ src/components/IntakeForm/Inputs/PeoplePicker.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import { usePeoplePicker } from "components/Hooks/usePeoplePicker";
+import {
+  Checkbox,
+  IBasePickerSuggestionsProps,
+  NormalPeoplePicker,
+  ValidationState,
+  IPersonaProps,
+} from "@fluentui/react";
+import { people, mru } from "@uifabric/example-data";
+const suggestionProps: IBasePickerSuggestionsProps = {
+  suggestionsHeaderText: "Suggested People",
+  mostRecentlyUsedHeaderText: "Suggested Contacts",
+  noResultsFoundText: "No results found",
+  loadingText: "Loading",
+  showRemoveButtons: true,
+  suggestionsAvailableAlertText: "People Picker Suggestions available",
+  suggestionsContainerAriaLabel: "Suggested contacts",
+};
+
+// const onFilterChanged = (
+//   filterText: string,
+//   currentPersonas: IPersonaProps[],
+//   limitResults?: number
+// ): IPersonaProps[] | Promise<IPersonaProps[]> => {
+//   console.log(
+//     "filterText:",
+//     filterText,
+//     "currentPersonas:",
+//     currentPersonas,
+//     "limitResults:",
+//     limitResults
+//   );
+//   if (filterText) {
+//     onChange(filterText);
+//     return [];
+//   } else {
+//     return [];
+//   }
+// };
+
+console.log(`people`, people);
+export const PeoplePicker: React.FunctionComponent = () => {
+  const { onChange, searchResults, reset } = usePeoplePicker();
+
+  return (
+    <NormalPeoplePicker
+      // @ts-ignore
+      onResolveSuggestions={onChange}
+      // // @ts-ignore
+      // onEmptyInputFocus={returnMostRecentlyUsed}
+      // getTextFromItem={getTextFromItem}
+      pickerSuggestionsProps={suggestionProps}
+      // className={"ms-PeoplePicker"}
+      key={"normal"}
+      // // eslint-disable-next-line react/jsx-no-bind
+      // onRemoveSuggestion={onRemoveSuggestion}
+      // onValidateInput={validateInput}
+      removeButtonAriaLabel={"Remove"}
+      // inputProps={{
+      //   onBlur: (ev: React.FocusEvent<HTMLInputElement>) =>
+      //     console.log("onBlur called"),
+      //   onFocus: (ev: React.FocusEvent<HTMLInputElement>) =>
+      //     console.log("onFocus called"),
+      //   "aria-label": "People Picker",
+      // }}
+      // componentRef={picker}
+      // onInputChange={onInputChange}
+      // resolveDelay={300}
+      // disabled={isPickerDisabled}
+    />
+  );
+};

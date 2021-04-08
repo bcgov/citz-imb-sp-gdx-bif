fe32ee16e5d1f2b5996076e85afbf6fd058c57ca
SDToews <33942967+SDToews@users.noreply.github.com>
2021-04-08 10:30:48 -0700

    remove logs and add keys to mapped elements

--- src/components/IntakeForm/formSchema.tsx
+++ src/components/IntakeForm/formSchema.tsx
@@ -1,22 +1,22 @@
 import * as Yup from 'yup';
 
 export const formSchema = (columns: any) => {
-  console.log(`columns`, columns);
-  let tempSchema: any = {};
-  for (let i = 0; i < columns.length; i++) {
-    if (columns[i].fieldTypeKind === 20) {
-      if (columns[i].required) {
-        tempSchema[columns[i].fieldName] = Yup.array()
-          .min(1, columns[i].name + ' is required')
-          .required(columns[i].name + ' is required');
-      }
-    } else {
-      if (columns[i].required) {
-        tempSchema[columns[i].fieldName] = Yup.string().required(
-          columns[i].name + ' is required'
-        );
-      }
-    }
-  }
-  return Yup.object().shape(tempSchema);
+	let tempSchema: any = {};
+
+	for (let i = 0; i < columns.length; i++) {
+		if (columns[i].fieldTypeKind === 20) {
+			if (columns[i].required) {
+				tempSchema[columns[i].fieldName] = Yup.array()
+					.min(1, columns[i].name + ' is required')
+					.required(columns[i].name + ' is required');
+			}
+		} else {
+			if (columns[i].required) {
+				tempSchema[columns[i].fieldName] = Yup.string().required(
+					columns[i].name + ' is required'
+				);
+			}
+		}
+	}
+	return Yup.object().shape(tempSchema);
 };
